55739121de4c560149024ea8c0448bbca01b9ce5
Nure Alam <nalam@redionstechnologies.com>
2020-01-16 16:23:18 +0530

    commiting after implementing google login

--- EmployeeService/App_Start/Startup.Auth.cs
+++ EmployeeService/App_Start/Startup.Auth.cs
@@ -38,7 +38,7 @@ namespace EmployeeService
                 TokenEndpointPath = new PathString("/Token"),
                 Provider = new ApplicationOAuthProvider(PublicClientId),
                 AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
-                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+                AccessTokenExpireTimeSpan = TimeSpan.FromSeconds(60),
                 // In production mode set AllowInsecureHttp = false
                 AllowInsecureHttp = true
             };
@@ -59,11 +59,11 @@ namespace EmployeeService
             //    appId: "",
             //    appSecret: "");
 
-            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
-            //{
-            //    ClientId = "",
-            //    ClientSecret = ""
-            //});
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = "593133331439-643pju933hogn1en8j9ncog9drtv1uki.apps.googleusercontent.com",
+                ClientSecret = "bmnxUvfLfJ7UyLMbua-5neMl"
+            });
         }
     }
 }
